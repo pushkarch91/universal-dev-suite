c874b9d5f200baca06c506ae6f194b3d774572af
pushkarchauhan91 <pushkarchauhan91@pushkarchauhan91s-MacBook-Air.local>
2025-04-30 11:12:04 +0530

    leetcode

---
+++ leetcode/leetcode-java/src/main/java/com/company/problem0412/fizzbuzz/P0412FizzBuzz.java
@@ -0,0 +1,34 @@
+package com.company.problem0412.fizzbuzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P0412FizzBuzz {
+
+    public static void main(String[] args) {
+        P0412FizzBuzz p = new P0412FizzBuzz();
+        System.out.println(p.fizzBuzz(5));
+        System.out.println(p.fizzBuzz(15));
+        System.out.println(p.fizzBuzz(25));
+        System.out.println(p.fizzBuzz(30));
+        System.out.println(p.fizzBuzz(35));
+        System.out.println(p.fizzBuzz(45));
+        System.out.println(p.fizzBuzz(55));
+    }
+
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(i + "");
+            }
+        }
+        return result;
+    }
+}
