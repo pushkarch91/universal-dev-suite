0bb434226e966cd5c7f0739f4a2fba26b45256ac
pushkarchauhan91 <pushkarchauhan91@pushkarchauhan91s-MacBook-Air.local>
2025-08-11 08:54:33 +0530

    code

--- java-maven/parent-maven/version/test/spring/project-multiple-database/src/main/java/com/company/projectmultipledatabase/orderrepository/OrderDao.java
+++
@@ -1,7 +0,0 @@
-package com.company.projectmultipledatabase.orderrepository;
-
-import com.company.projectmultipledatabase.orderentity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDao extends JpaRepository<Order, Integer> {
-}
--- java-maven/parent-maven/version/test/spring/project-multiple-database/src/main/java/com/company/projectmultipledatabase/productrepository/ProductDao.java
+++
@@ -1,7 +0,0 @@
-package com.company.projectmultipledatabase.productrepository;
-
-import com.company.projectmultipledatabase.productentity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductDao extends JpaRepository<Product, Integer> {
-}
